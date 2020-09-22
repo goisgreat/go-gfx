70bdf72a425608e04504b0bea714139a8f2042d2
Addison <addison.brendtro@gmail.com>
2020-09-22 10:02:50 -0500

    Fixed bug with `iota` enum declaration

--- constants.go
+++ constants.go
@@ -1,10 +1,13 @@
 package physics
 
+// x-y borders
 const (
-	// x-y borders
 	MAX_X int = 7 // max X coordinate for an object
 	MAX_Y int = 7 // max Y coordinate for an object
-	// ENUM: keyboard controls
+)
+
+// ENUM: keyboard controls
+const (
 	CON_UP KeyboardControl = iota
 	CON_LEFT
 	CON_DOWN
