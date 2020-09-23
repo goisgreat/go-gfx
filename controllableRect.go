0aa0a56e71c3f808ea076b7766659cfa590e2612
Addison <addison.brendtro@gmail.com>
2020-09-23 14:10:00 -0500

    Removed redundant Update() method on ControllableRect

--- controllableRect.go
+++ controllableRect.go
@@ -6,13 +6,3 @@ type ControllableRect struct {
 	Rect
 	Callbacks
 }
-
-/*
- * update given ControllableRect
- */
-func (controllableRect ControllableRect) Update() (Shape, bool) {
-	// update using "callbacks" object
-	controllableRect.Callbacks.Update()
-	// return shape
-	return &controllableRect.Rect, true
-}
