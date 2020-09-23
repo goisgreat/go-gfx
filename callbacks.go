fa9bf58ae836d4122fe6db134a3c0ec8a3702622
Addison <addison.brendtro@gmail.com>
2020-09-23 13:26:30 -0500

    Changed Callbacks.Update() to return (Shape, bool)

--- callbacks.go
+++ callbacks.go
@@ -1,15 +1,18 @@
 package physics
 
 type Callbacks struct {
-	OnUpdate func()
+	OnUpdate func() (Shape, bool)
 }
 
 /*
  * use stored callbacks to update
  */
-func (callbacks Callbacks) Update() {
-	//
+func (callbacks Callbacks) Update() (Shape, bool) {
+	// if onupdate is set, invoke it and return shape, bool
 	if callbacks.OnUpdate != nil {
-		callbacks.OnUpdate()
+		shape, hasShape := callbacks.OnUpdate()
+		return shape, hasShape
 	}
+	// otherwise return no shape and false
+	return nil, false
 }
