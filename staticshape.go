4e646e0fe3cab265524c370e5e884e31d18f8f8d
Addison <addison.brendtro@gmail.com>
2020-09-23 16:15:30 -0500

    Made StaticShape implement Sprite

--- staticshape.go
+++ staticshape.go
@@ -5,3 +5,11 @@ type StaticShape struct {
 	Shape
 	CollisionBox
 }
+
+func (staticShape *StaticShape) Update() (Shape, bool) {
+	return staticShape.Shape, true
+}
+
+func (staticShape *StaticShape) Init() {
+
+}
