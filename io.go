f0a597e3314aa1f0104a6599bb744522f4de6367
Addison <addison.brendtro@gmail.com>
2020-09-23 16:27:45 -0500

    Moved terminal-related methods to terminal.go

--- io.go
+++ io.go
@@ -2,24 +2,7 @@ package physics
 
 import (
 	"os"
-	"os/exec"
 )
-
-/*
- * disable input buffering from stdin (so that you don't have to press ENTER)
- */
-func DisableStdinBuffer() {
-	// disable input buffering
-	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-}
-
-/*
- * disable entered characters from being "echo"ed
- */
-func DisableStdinEcho() {
-	// do not display entered characters on the screen
-	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-}
 
 /*
  * usage: call this function, get a callback, invoke the callback, and a byte or error will be returned
--- terminal.go
+++ terminal.go
@@ -1,8 +1,26 @@
 package physics
 
+import "os/exec"
+
 /*
  * clear the terminal using ascii escape sequences
  */
 func ClearTerminal() {
 	print("\033[H\033[2J")
+}
+
+/*
+ * disable input buffering from stdin (so that you don't have to press ENTER)
+ */
+func DisableStdinBuffer() {
+	// disable input buffering
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+}
+
+/*
+ * disable entered characters from being "echo"ed
+ */
+func DisableStdinEcho() {
+	// do not display entered characters on the screen
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
 }
