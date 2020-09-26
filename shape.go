f86f159f27a68fa49c54799087bd8f5bda54b9c9
Addison <addison.brendtro@gmail.com>
2020-09-25 20:02:07 -0500

    Added Overlaps() method to Rectangle

--- shape.go
+++ shape.go
@@ -4,8 +4,10 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
+	"os"
 )
 
+// type Shape
 type Shape interface {
 	Draw(draw.Image)
 	Overlaps(Shape) bool
@@ -17,7 +19,8 @@ type Rectangle struct {
 	Color  color.RGBA      // color
 }
 
-// Draw() draws, pixel by pixel, a rectangle
+// Draw() satisfies the Draw method on interface Shape
+// it draws, pixel by pixel, a rectangle
 func (rectangle Rectangle) Draw(frame draw.Image) {
 	// variables
 	var x int = rectangle.Bounds.Min.X // current x coordinate
@@ -35,4 +38,17 @@ func (rectangle Rectangle) Draw(frame draw.Image) {
 		// increment
 		y++
 	}
+}
+
+// Overlaps() satisfies the Overlaps method on interface Shape
+// it reports whether a rectangle overlaps with a given shape
+func (rectangle Rectangle) Overlaps(shape Shape) bool {
+	if rectangle1, ok := shape.(*Rectangle); ok { // we are processing a rectangle.
+		return rectangle.Bounds.Overlaps(rectangle1.Bounds)
+	}
+	// we are processing an unknown shape and we freak out.
+	println("Moving unknown shape")
+	os.Exit(1)
+	// unreachable code but we still need to return a boolean
+	return false
 }
--- vector.go
+++ vector.go
@@ -11,10 +11,10 @@ type Vector image.Point
 // Hit() moves [shape] using the X/Y coordinates from a vector
 func (vector Vector) Hit(shape Shape) {
 	if rect, ok := shape.(*Rectangle); ok { // we are moving a rectangle.
-		rect.Start.X += vector.X
-		rect.Start.Y += vector.Y
-		rect.End.X += vector.X
-		rect.End.Y += vector.Y
+		rect.Bounds.Min.X += vector.X
+		rect.Bounds.Min.Y += vector.Y
+		rect.Bounds.Max.X += vector.X
+		rect.Bounds.Max.Y += vector.Y
 		return
 	} else { // we are moving an unknown shape and we freak out.
 		println("Moving unknown shape")
