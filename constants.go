1bd681af623a2f90c3cdb7a413f94e821da8f728
Addison <addison.brendtro@gmail.com>
2020-09-26 09:43:29 -0500

    Removed MAX_X and MAX_Y constants

--- constants.go
+++ constants.go
@@ -1,11 +1,5 @@
 package physics
 
-// x-y borders
-const (
-	MAX_X int = 7 // max X coordinate for an object
-	MAX_Y int = 7 // max Y coordinate for an object
-)
-
 // ENUM: keyboard controls
 const (
 	CON_UP KeyboardControl = iota
