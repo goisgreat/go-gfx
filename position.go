bf04284e796f76479ed0d8fbc544182b6b5663fe
Addison <addison.brendtro@gmail.com>
2020-09-26 08:55:40 -0500

    Removed Position.AssertBounds

--- position.go
+++ position.go
@@ -17,23 +17,3 @@ func (position Position) Draw(frame draw.Image) {
 	// set position on frame
 	frame.Set(position.Coordinates.X, position.Coordinates.Y, position.Color)
 }
-
-/*
- * assert bounds on a given position
- * if position exceeds bounds, wrap
- */
-func (position *Position) AssertBounds() {
-	// if position.Coordinates.X < 0, wrap to {WIDTH}
-	if position.Coordinates.X < 0 {
-		position.Coordinates.X = MAX_X
-	} else if position.Coordinates.X > MAX_X { // if position.Coordinates.X > WIDTH, wrap to 0
-		position.Coordinates.X = 0
-	}
-
-	// if position.Coordinates.Y < 0, wrap to {HEIGHT}
-	if position.Coordinates.Y < 0 {
-		position.Coordinates.Y = MAX_Y
-	} else if position.Coordinates.Y > MAX_Y { // if position.Coordinates.Y > HEIGHT, wrap to 0
-		position.Coordinates.Y = 0
-	}
-}
